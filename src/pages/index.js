e2e2db788b1d401dea1e6fe09abc63275e59d0f0
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-11-07 22:12:48 +0700

    [UPDATE] Update navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,22 @@
 export default function Home() {
   return (
-    <main className="home-page">
+    <main id="home-page">
+      {/* Header start */}
       <header className="container mx-auto">
+        {/* Navbar */}
         <nav className="flex justify-between py-5">
           <img
             src="/images/logo/peworld-main.svg"
             alt="logo"
             className="w-[127px] h-[35px]"
           />
-          <div className="flex gap-4 auth-btn">
-            <button className="border border-[#5E50A1] text-[#5E50A1] text-sm font-bold px-4 rounded">
-              Masuk
-            </button>
-            <button className="bg-[#5E50A1] border border-[#5E50A1] text-[#fff] text-sm font-bold px-4 rounded">
-              Daftar
-            </button>
+          <div className="flex gap-4">
+            <button className="btn-login px-5">Masuk</button>
+            <button className="btn-register px-5">Daftar</button>
           </div>
         </nav>
       </header>
+      {/* Header end */}
     </main>
   );
 }
