c84e2c3340c83c937fe946fccacf44e27151186a
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-11-08 02:42:18 +0700

    [UPDATE] Create hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
       {/* Header start */}
       <header className="container mx-auto">
         {/* Navbar */}
-        <nav className="flex justify-between py-5">
+        <nav className="flex justify-between py-8 mobile-container">
           <img
             src="/images/logo/peworld-main.svg"
             alt="logo"
@@ -15,6 +15,27 @@ export default function Home() {
             <button className="btn-register px-5">Daftar</button>
           </div>
         </nav>
+        {/* Hero Content */}
+        <section className="grid grid-cols-1 md:grid-cols-2 items-center mt-10 mobile-container">
+          <div className="col-span-1">
+            <h1 className="hero-primary">
+              Talenta terbaik negri untuk perubahan revolusi 4.0
+            </h1>
+            <p className="hero-secondary mt-5">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+              euismod ipsum et dui rhoncus auctor.
+            </p>
+            <button className="mt-5 hero-button px-5 py-5">
+              Mulai Dari Sekarang
+            </button>
+          </div>
+          <div className="col-span-1 flex justify-end">
+            <img
+              src="/images/homepage-image/hero-content.png"
+              alt="hero-image"
+            />
+          </div>
+        </section>
       </header>
       {/* Header end */}
     </main>
