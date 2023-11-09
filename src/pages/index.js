b7920e8bf86618043e898ee1933790e0aa1e2e88
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-11-09 11:21:30 +0700

    [UPDATE] Update Navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,43 @@
+import { Open_Sans } from "next/font/google";
+
+const openSans = Open_Sans({
+  subsets: ["latin"],
+});
+
 export default function Home() {
   return (
     <main id="home-page">
       {/* Header start */}
       <header className="container mx-auto">
         {/* Navbar */}
-        <nav className="flex justify-between py-8 mobile-container">
+        <nav className="flex justify-between py-5 mobile-container">
           <img
             src="/images/logo/peworld-main.svg"
             alt="logo"
-            className="w-[127px] h-[35px]"
+            className="w-[7.9375rem] h-[2.1875rem]"
           />
           <div className="flex gap-4">
-            <button className="btn-login px-5">Masuk</button>
-            <button className="btn-register px-5">Daftar</button>
+            <button
+              className={`${openSans.className} border border-[#5e50a1] rounded px-5 text-[#5e50a1] text-sm font-bold leading-6`}
+            >
+              Masuk
+            </button>
+            <button
+              className={`${openSans.className} bg-[#5e50a1] rounded px-5 text-[#fff] text-sm font-bold leading-6`}
+            >
+              Daftar
+            </button>
           </div>
         </nav>
         {/* Hero Content */}
-        <section className="grid grid-cols-1 md:grid-cols-2 items-center mt-10 mobile-container">
-          <div className="col-span-1">
+        {/* <section className="grid grid-cols-1 md:grid-cols-3 items-center mt-10 mobile-container">
+          <div className="col-span-2">
             <h1 className="hero-primary">
-              Talenta terbaik negri untuk perubahan revolusi 4.0
+              Talenta terbaik negri
+              <br />
+              untuk perubahan
+              <br />
+              revolusi 4.0
             </h1>
             <p className="hero-secondary mt-5">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
@@ -29,13 +47,13 @@ export default function Home() {
               Mulai Dari Sekarang
             </button>
           </div>
-          <div className="col-span-1 flex justify-end">
+          <div className="col-span-1">
             <img
               src="/images/homepage-image/hero-content.png"
               alt="hero-image"
             />
           </div>
-        </section>
+        </section> */}
       </header>
       {/* Header end */}
     </main>
