5623710447f86647e9c4203178cfdd8750ce4016
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-11-12 20:32:58 +0700

    [UPDATE] Add navbar in detail id pages

--- src/pages/talent-list/detail/[id].js
+++ src/pages/talent-list/detail/[id].js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Open_Sans } from "next/font/google";
+import Image from "next/image";
+import Head from "next/head";
+import axios from "axios";
+import { useRouter } from "next/router";
+
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+const openSans = Open_Sans({
+  subsets: ["latin"],
+});
 
 function Detail() {
-  return <p>TEST</p>;
+  return (
+    <main>
+      {/* Header start */}
+      <header className="container mx-auto px-2">
+        {/* Navbar */}
+        <Navbar />
+      </header>
+      {/* Header end */}
+    </main>
+  );
 }
 
 export default Detail;
