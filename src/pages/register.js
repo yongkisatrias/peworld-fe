1775cb76bbbe54cf01516dc45ce3c5ed2a2d3606
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-11-17 15:16:49 +0700

    [UPDATE] Update head title register page

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Head from "next/head";
 
 function Register() {
-  return <></>;
+  return (
+    <div>
+      <Head>
+        <title>Peworld | Register</title>
+      </Head>
+      <div></div>
+    </div>
+  );
 }
 
 export default Register;
