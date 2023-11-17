f9ab4b74bf6b8c89cd8bf955165a220536575cef
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-11-17 15:17:39 +0700

    [UPDATE] Update font register page

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Head from "next/head";
+import { Open_Sans } from "next/font/google";
+
+const openSans = Open_Sans({
+  subsets: ["latin"],
+});
 
 function Register() {
   return (
-    <div>
+    <div className={openSans.className}>
       <Head>
         <title>Peworld | Register</title>
       </Head>
