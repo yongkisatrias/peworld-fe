ffe377d8f1f0b64e792fa30b3c9d3cb2f81db3d0
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-11-17 15:20:41 +0700

    [UPDATE] Update left section register page

--- src/pages/register.js
+++ src/pages/register.js
@@ -12,7 +12,26 @@ function Register() {
       <Head>
         <title>Peworld | Register</title>
       </Head>
-      <div></div>
+      <div className="grid grid-cols-4">
+        {/* Left Section */}
+        <div className="col-span-4 md:col-span-2 relative hidden md:block">
+          <div className="absolute bg-[#5E50A1] w-[100%] h-[100vh] opacity-[.60]" />
+          <img
+            src="/images/auth/auth.png"
+            className="w-[100%] h-[100vh] object-cover"
+            alt="background-image"
+          />
+          <img
+            src="/images/logo/peworld-white.svg"
+            className="absolute inset-0 w-[10rem] h-[5rem] p-4"
+          />
+          <div className="absolute flex justify-center items-center inset-0">
+            <h1 className="text-white text-5xl font-bold leading-[4.375rem] px-32">
+              Find talented & best developers in various areas of expertise.
+            </h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
