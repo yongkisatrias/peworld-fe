e789088023d8cbf65d3c4cec321747cd0b4702cb
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-11-18 13:29:00 +0700

    [UPDATE] Add navbar in contact id pages

--- src/pages/talent-list/contact/[id].js
+++ src/pages/talent-list/contact/[id].js
@@ -1,13 +1,20 @@
 import React from "react";
 import Head from "next/head";
 
+import Navbar from "@/components/Navbar";
+
 function ContactPage() {
   return (
     <div>
       <Head>
         <title>Peworld | Contact</title>
       </Head>
-      <div></div>
+      {/* Header start */}
+      <header className="container mx-auto px-2">
+        {/* Navbar */}
+        <Navbar />
+      </header>
+      {/* Header end */}
     </div>
   );
 }
