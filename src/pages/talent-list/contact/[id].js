04b7937b9bfbf58ffde3bc8fe69373b8257b55e8
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-11-18 13:26:11 +0700

    [UPDATE] Add head tiitle in contact id pages

--- src/pages/talent-list/contact/[id].js
+++ src/pages/talent-list/contact/[id].js
@@ -1,8 +1,12 @@
 import React from "react";
+import Head from "next/head";
 
 function ContactPage() {
   return (
     <div>
+      <Head>
+        <title>Peworld | Contact</title>
+      </Head>
       <div></div>
     </div>
   );
