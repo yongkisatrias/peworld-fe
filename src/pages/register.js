79bf394eccc82a1bb9dabf07aef6ef4a58aeeb51
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-11-20 01:55:56 +0700

    [UPDATE] Integration API register page

--- src/pages/register.js
+++ src/pages/register.js
@@ -2,12 +2,57 @@ import React from "react";
 import Head from "next/head";
 import { Open_Sans } from "next/font/google";
 import Link from "next/link";
+import axios from "axios";
 
 const openSans = Open_Sans({
   subsets: ["latin"],
 });
 
 function Register() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [fullname, setFullname] = React.useState("");
+  const [company, setCompany] = React.useState("");
+  const [job, setJob] = React.useState("");
+  const [phone, setPhone] = React.useState("");
+  const [errorMessage, setErrorMessage] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const [registerSuccess, setRegisterSuccess] = React.useState(null);
+
+  const registerHandler = () => {
+    setLoading(true);
+    axios({
+      method: "post",
+      url: `https://hire-job.onrender.com/v1/auth/register`,
+      data: {
+        email: email,
+        password: password,
+        fullname: fullname,
+        company: company,
+        job_title: job,
+        phone: phone,
+      },
+    })
+      .then((result) => {
+        console.log(result);
+
+        setRegisterSuccess(true);
+      })
+      .catch((err) => {
+        console.log(err);
+
+        const errFullname = err?.response?.data?.messages?.fullname?.message;
+        const errEmail = err?.response?.data?.messages?.email?.message;
+        const errPassword = err?.response?.data?.messages?.password?.message;
+        const errPhone = err?.response?.data?.messages?.phone?.message;
+
+        setErrorMessage(errFullname ?? errEmail ?? errPassword ?? errPhone);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   return (
     <div className={openSans.className}>
       <Head>
@@ -40,6 +85,19 @@ function Register() {
           <p className="text_secondary text-lg font-normal py-1">
             Please complete your data to register on the peworld website.
           </p>
+          {errorMessage ? (
+            <div className="bg-[#f8d7da] text-[#721c24] px-4 py-3 my-4 rounded">
+              <p className="p-2">{errorMessage}</p>
+            </div>
+          ) : (
+            false
+          )}
+
+          {registerSuccess ? (
+            <div className="bg-[#d1e7dd] text-[#0f5132] px-4 py-3 my-4 rounded">
+              <p className="p-2">Register Success</p>
+            </div>
+          ) : null}
           <label
             className="text-[#9EA0A5] text-sm font-normal mb-2 mt-4"
             htmlFor="name"
@@ -51,6 +109,7 @@ function Register() {
             className="h-[3.125rem] rounded border border-[#E2E5ED] pl-4 text-[#858D96] text-base font-normal"
             placeholder="Enter a full name"
             type="text"
+            onChange={(item) => setFullname(item.target.value)}
           />
           <label
             className="text-[#9EA0A5] text-sm font-normal mb-2 mt-4"
@@ -63,6 +122,7 @@ function Register() {
             className="h-[3.125rem] rounded border border-[#E2E5ED] pl-4 text-[#858D96] text-base font-normal"
             placeholder="Enter email address"
             type="email"
+            onChange={(item) => setEmail(item.target.value)}
           />
           <label
             className="text-[#9EA0A5] text-sm font-normal mb-2 mt-4"
@@ -75,6 +135,7 @@ function Register() {
             className="h-[3.125rem] rounded border border-[#E2E5ED] pl-4 text-[#858D96] text-base font-normal"
             placeholder="Enter the company name"
             type="text"
+            onChange={(item) => setCompany(item.target.value)}
           />
           <label
             className="text-[#9EA0A5] text-sm font-normal mb-2 mt-4"
@@ -87,6 +148,7 @@ function Register() {
             className="h-[3.125rem] rounded border border-[#E2E5ED] pl-4 text-[#858D96] text-base font-normal"
             placeholder="Position in your company"
             type="text"
+            onChange={(item) => setJob(item.target.value)}
           />
           <label
             className="text-[#9EA0A5] text-sm font-normal mb-2 mt-4"
@@ -99,6 +161,7 @@ function Register() {
             className="h-[3.125rem] rounded border border-[#E2E5ED] pl-4 text-[#858D96] text-base font-normal"
             placeholder="Enter your phone number"
             type="text"
+            onChange={(item) => setPhone(item.target.value)}
           />
           <label
             className="text-[#9EA0A5] text-sm font-normal mb-2 mt-4"
@@ -111,6 +174,7 @@ function Register() {
             className="h-[3.125rem] rounded border border-[#E2E5ED] pl-4 text-[#858D96] text-base font-normal"
             placeholder="Enter password"
             type="password"
+            onChange={(item) => setPassword(item.target.value)}
           />
           <label
             className="text-[#9EA0A5] text-sm font-normal mb-2 mt-4"
@@ -124,8 +188,12 @@ function Register() {
             placeholder="Enter password confirmation"
             type="password"
           />
-          <button className="bg-[#FBB017] rounded py-3 my-10 text-white text-base font-bold">
-            Login
+          <button
+            className="bg-[#FBB017] rounded py-3 my-10 text-white text-base font-bold"
+            onClick={() => registerHandler()}
+            disabled={loading}
+          >
+            {loading ? "Loading..." : "Login"}
           </button>
           <p className="text-center text-primary text-base font-normal mt-4">
             Do you already have an account?{" "}
